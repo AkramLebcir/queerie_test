88c568988c695c0be0f2281223c023693830a1dc
lebcir.akram <akramlebcir@gmail.com>
2025-07-22 15:09:27 +0200

    add initial configuration for multiple build flavors and permissions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.akramlebcir.queerie_test"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -35,6 +35,24 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    flavorDimensions += "env"
+
+    productFlavors {
+        create("sandbox") {
+            dimension = "env"
+            versionNameSuffix = "-sandbox"
+            applicationIdSuffix = ".sandbox"
+        }
+        create("staging") {
+            dimension = "env"
+            versionNameSuffix = "-staging"
+            applicationIdSuffix = ".staging"
+        }
+        create("prod") {
+            dimension = "env"
         }
     }
 }
